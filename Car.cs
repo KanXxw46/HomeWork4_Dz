039a56fab873a4289592032f76579bd8bb85fa8f
KanXxw46 <1kan_andreikkkk@mail.ru>
2020-06-03 22:43:26 +0600

    Dz

---
+++ Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace HomeWork4_Dz
+{
+    class Car
+    {
+        private string number;
+        private string color;
+        private string model;
+        private int speed;
+        private int price;
+        public int size { get; set; }
+        public DateTime date { get; set; }
+        public string wheel { get; set; }
+        public override string ToString()
+        {
+            return "Name: {0}\n Size: {1}\n Date: {2}\n wheel: {3}\n" +  + size + date + wheel;
+        }
+
+        public string CarNumber
+        {
+            get
+            {
+                return number;
+            }
+        }
+        public string CarColor
+        {
+            get
+            {
+                return color;
+            }
+        }
+        public string CarModel
+        {
+            get
+            {
+                return model;
+            }
+        }
+        public int CarSpeed
+        {
+            get
+            {
+                return speed;
+            }
+        }
+        public int CarPrice
+        {
+            get
+            {
+                return price;
+            }
+        }
+        public static int Type;
+        public static int Power;
+
+        static Car()
+        {
+            Console.WriteLine("Car in Shop");
+        }
+        public static int Cars = 10;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork4_Dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
